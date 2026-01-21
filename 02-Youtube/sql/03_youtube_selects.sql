9347c3cd3b815f6d9e1b7d0dc6b248bd1d64f928
Elisa Rumeysa Solberg <68196918+ElisaRumSolberg@users.noreply.github.com>
2026-01-21 10:32:16 +0100

    Add select queries

--- 02-Youtube/sql/03_youtube_selects.sql
+++ 02-Youtube/sql/03_youtube_selects.sql
@@ -17,3 +17,34 @@ WHERE score > 500;
 SELECT *
 FROM youtube.customers
 WHERE country = 'Germany';
+
+----retrieve all customers and sort the results by the lowest score first
+SELECT *
+FROM youtube.customers
+ORDER BY score ASC;
+--retrieve all customers and sort the results by the hugest score first
+SELECT *
+FROM youtube.customers
+ORDER BY score  DESC ;
+
+--retrieve all customers and sort the results by the country
+-- and by the highest score first
+SELECT *
+FROM youtube.customers
+ORDER BY
+    country ASC,
+    score DESC;
+
+--WHERE + ORDER BY birlikte
+--Filtrele → sonra sırala:
+
+SELECT first_name, score
+FROM youtube.customers
+WHERE score > 0
+ORDER BY score DESC;
+
+-- orders: satis miktarina gore siralama
+SELECT order_id, sales
+FROM youtube.orders
+ORDER BY sales DESC;
+
